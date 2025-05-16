bf73907c2e5f9ddbaab72fa16ed8210c0ee5c6f8
RuihanGao <ruihang@andrew.cmu.edu>
2025-05-16 00:56:59 -0400

    update bash script to run for new prompts 20250515

--- run_inference.sh
+++ run_inference.sh
@@ -1,4 +1,15 @@
-CUDA_VISIBLE_DEVICES=1 python -m scripts.inference_midi --rgb assets/data/midi_images_simplified_prompts/fruit_basket.jpeg --seg output/fruit_basket_seg.png --output-dir "./output" & 
-CUDA_VISIBLE_DEVICES=2 python -m scripts.inference_midi --rgb assets/data/midi_images_simplified_prompts/headphone_stand.png --seg output/headphone_stand_seg.png --output-dir "./output" &
-CUDA_VISIBLE_DEVICES=3 python -m scripts.inference_midi --rgb assets/data/midi_images_simplified_prompts/stacked_cups.png --seg output/stacked_cups_seg.png --output-dir "./output" 
-CUDA_VISIBLE_DEVICES=1 python -m scripts.inference_midi --rgb assets/data/midi_images_simplified_prompts/vase_table.png --seg output/vase_table_seg.png --output-dir "./output"
+# # 2025-05-01. Test on 4 simplified prompts
+# CUDA_VISIBLE_DEVICES=1 python -m scripts.inference_midi --rgb assets/data/midi_images_simplified_prompts/fruit_basket.jpeg --seg output/fruit_basket_seg.png --output-dir "./output" & 
+# CUDA_VISIBLE_DEVICES=2 python -m scripts.inference_midi --rgb assets/data/midi_images_simplified_prompts/headphone_stand.png --seg output/headphone_stand_seg.png --output-dir "./output" &
+# CUDA_VISIBLE_DEVICES=3 python -m scripts.inference_midi --rgb assets/data/midi_images_simplified_prompts/stacked_cups.png --seg output/stacked_cups_seg.png --output-dir "./output" 
+# CUDA_VISIBLE_DEVICES=1 python -m scripts.inference_midi --rgb assets/data/midi_images_simplified_prompts/vase_table.png --seg output/vase_table_seg.png --output-dir "./output"
+
+
+# 2025-05-15. Test on 13 prompts
+CUDA_VISIBLE_DEVICES=0 python -m scripts.inference_midi --rgb assets/data/midi_images_20250515/07_vase_table.png --seg output/07_vase_table_seg.png --output-dir "./output"
+CUDA_VISIBLE_DEVICES=0 python -m scripts.inference_midi --rgb assets/data/midi_images_20250515/08_fruit_basket.png --seg output/08_fruit_basket_seg.png --output-dir "./output"
+CUDA_VISIBLE_DEVICES=0 python -m scripts.inference_midi --rgb assets/data/midi_images_20250515/09_cups_plates.png --seg output/09_cups_plates_seg.png --output-dir "./output"
+CUDA_VISIBLE_DEVICES=0 python -m scripts.inference_midi --rgb assets/data/midi_images_20250515/10_toothbrush_soap.png --seg output/10_toothbrush_soap_seg.png --output-dir "./output"
+CUDA_VISIBLE_DEVICES=0 python -m scripts.inference_midi --rgb assets/data/midi_images_20250515/11_radio_books.png --seg output/11_radio_books_seg.png --output-dir "./output"
+CUDA_VISIBLE_DEVICES=0 python -m scripts.inference_midi --rgb assets/data/midi_images_20250515/12_sofa_bear.png --seg output/12_sofa_bear_seg.png --output-dir "./output"
+CUDA_VISIBLE_DEVICES=0 python -m scripts.inference_midi --rgb assets/data/midi_images_20250515/13_pool_balls.png --seg output/13_pool_balls_seg.png --output-dir "./output"
--- run_segmentation.sh
+++ run_segmentation.sh
@@ -1,5 +1,18 @@
 # Note: need to manually provide the labels
-python -m scripts.grounding_sam --image assets/data/midi_images_simplified_prompts/fruit_basket.jpeg --labels fruit basket --output ./output
-python -m scripts.grounding_sam --image assets/data/midi_images_simplified_prompts/headphone_stand.png --labels headphone stand --output ./output
-python -m scripts.grounding_sam --image assets/data/midi_images_simplified_prompts/stacked_cups.png --labels cup --output ./output
-python -m scripts.grounding_sam --image assets/data/midi_images_simplified_prompts/vase_table.png --labels vase cake plate fork table --output ./output
+
+# # 2025-05-01. Test on 4 simplified prompts
+# python -m scripts.grounding_sam --image assets/data/midi_images_simplified_prompts/fruit_basket.jpeg --labels fruit basket --output ./output
+# python -m scripts.grounding_sam --image assets/data/midi_images_simplified_prompts/headphone_stand.png --labels headphone stand --output ./output
+# python -m scripts.grounding_sam --image assets/data/midi_images_simplified_prompts/stacked_cups.png --labels cup --output ./output
+# python -m scripts.grounding_sam --image assets/data/midi_images_simplified_prompts/vase_table.png --labels vase cake plate fork table --output ./output
+
+# 2025-05-15. Test on 13 prompts
+CUDA_VISIBLE_DEVICES=0 python -m scripts.grounding_sam --image assets/data/midi_images_20250515/07_vase_table.png --labels vase table flower cake plate --output ./output
+CUDA_VISIBLE_DEVICES=0 python -m scripts.grounding_sam --image assets/data/midi_images_20250515/08_fruit_basket.png --labels fruit basket --output ./output
+CUDA_VISIBLE_DEVICES=0 python -m scripts.grounding_sam --image assets/data/midi_images_20250515/09_cups_plates.png --labels cup plate --output ./output
+CUDA_VISIBLE_DEVICES=0 python -m scripts.grounding_sam --image assets/data/midi_images_20250515/10_toothbrush_soap.png --labels soap dish toothpate toothbrush razor --output ./output
+CUDA_VISIBLE_DEVICES=0 python -m scripts.grounding_sam --image assets/data/midi_images_20250515/11_radio_books.png --labels radio cow book pencils cup --output ./output
+CUDA_VISIBLE_DEVICES=0 python -m scripts.grounding_sam --image assets/data/midi_images_20250515/12_sofa_bear.png --labels sofa plushies cushions --output ./output
+CUDA_VISIBLE_DEVICES=0 python -m scripts.grounding_sam --image assets/data/midi_images_20250515/13_pool_balls.png --labels pool balls toys --output ./output
+
+
